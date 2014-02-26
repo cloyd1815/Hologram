2b716f1363dbde2f3588fa5da1dcfa6586f2f583
Colin <loyd3152@gmail.com>
2014-02-26 11:10:34 -0500

    inital

--- src/me/cloyd1815/holo/Main.java
+++ src/me/cloyd1815/holo/Main.java
@@ -1,5 +1,21 @@
 package me.cloyd1815.holo;
 
-public class Main {
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin{
+	private static Main plugin;
+	@Override
+	public void onEnable() {
+		plugin = this;
+		
+	}
+	
+	@Override
+	public void onDisable() {
+		
+	}
+
+	public static Main getPlugin() {
+		return plugin;
+	}
 }
---
+++ src/me/cloyd1815/holo/commands/Add.java
@@ -0,0 +1,34 @@
+package me.cloyd1815.holo.commands;
+
+import me.cloyd1815.holo.util.Hologram;
+import net.minecraft.util.org.apache.commons.lang3.StringUtils;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Add implements CommandExecutor {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd,
+			String commandLabel, String[] args) {
+		Player player = (Player) sender;
+		if (commandLabel.equalsIgnoreCase("add")) {
+			if (args.length <= 0)
+				player.sendMessage("Incorrect syntax! /add message");
+			if (args.length >= 2) {
+				StringBuilder sb = new StringBuilder();
+				for (int i = 1; i < args.length; i++) {
+					sb.append(args[i]);
+					if (i < args.length) {
+						sb.append(" ");
+					}
+				}
+				new Hologram(sb.toString().replaceAll("&", "§")).show(player
+						.getLocation());
+			}
+		}
+		return true;
+	}
+}
